40ff898801a2913e4e1980d77994a5bbf6e063c7
JaeMin Lee / 이재민 <ljm9017@naver.com>
2024-02-02 15:01:39 +0900

    Code review 2/2

--- src/class1/ClassStart1.java
+++ src/class1/ClassStart1.java
@@ -25,8 +25,10 @@ public class ClassStart1 {
         System.out.println("이름: " + student1Name + " 나이 :" + student1Age + " 성적: " + student1Grade);
         System.out.println("이름: " + student2Name + " 나이 :" + student2Age + " 성적: " + student2Grade);
 
-        /*학생 2명을 다루어야 하기 때문에 각각 다른 변수를 사용했다.
+        /*
+        학생 2명을 다루어야 하기 때문에 각각 다른 변수를 사용했다.
         * 이 코드의 문제는 학생이 늘어날 때 마다 변수를 추가로 선언해야 하고,
-        * 이런 문제를 어떻게 해결할 수 있을까? 앞서 배운 배열을 사용하면 문제를 해결할 수 있다.*/
+        * 이런 문제를 어떻게 해결할 수 있을까? 앞서 배운 배열을 사용하면 문제를 해결할 수 있다.
+        */
     }
 }
